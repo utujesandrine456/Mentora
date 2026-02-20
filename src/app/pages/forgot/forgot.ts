3331d36da5ca0559e5383ddf76fa382272f47cdc
utujesandrine456 <utujesandrine456@gmail.com>
2026-02-20 12:15:48 +0200

    Add src/app/pages/forgot/forgot.ts

---
+++ src/app/pages/forgot/forgot.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-forgot',
+  imports: [RouterLink],
+  templateUrl: './forgot.html',
+  styleUrl: './forgot.css',
+})
+export class Forgot {}
