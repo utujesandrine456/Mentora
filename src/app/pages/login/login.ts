b302be70717af73910ccf0b2952e13bf7c7a003c
utujesandrine456 <utujesandrine456@gmail.com>
2026-02-20 12:15:50 +0200

    Add src/app/pages/login/login.ts

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  imports: [RouterLink],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
